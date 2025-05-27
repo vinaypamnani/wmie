74bbe62e2be985b8b3e32815d3bff92eff3c3852
vinaypamnani <27870285+vinaypamnani@users.noreply.github.com>
2025-05-26 20:45:55 -0600

    Add WmiQueryTab view and integrate into MainWindow layout

---
+++ WmiExplorer/Presentation/Views/Tabs/WmiQueryTab.xaml.cs
@@ -0,0 +1,16 @@
+namespace WmiExplorer.Presentation.Views.Tabs
+{
+    /// <summary>
+    /// Interaction logic for WmiQueryTab.xaml
+    /// </summary>
+    public partial class WmiQueryTab : System.Windows.Controls.UserControl
+    {
+        public WmiQueryTab()
+        {
+            InitializeComponent();
+
+            // This View will use the DataContext inherited from MainWindow
+            // Implementation will be added later
+        }
+    }
+}
